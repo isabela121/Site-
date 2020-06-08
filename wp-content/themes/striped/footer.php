ca7ea8720a0f73d20386b8e36a7db959c696e3ac
isabela121 <isabela.analisesistema@gmail.com>
2020-06-08 14:37:09 -0300

    atualizaçao um

---
+++ wp-content/themes/striped/footer.php
@@ -0,0 +1,8 @@
+		<!-- Scripts -->
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+</html>
