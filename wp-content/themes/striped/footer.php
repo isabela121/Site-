5ff1d7afbf86b27f3aa45ff5416d6a28edb5fbf6
isabela121 <isabela.analisesistema@gmail.com>
2020-06-08 15:21:55 -0300

    atualizaçao dois

--- wp-content/themes/striped/footer.php
+++ wp-content/themes/striped/footer.php
@@ -1,6 +1,5 @@
-		<!-- Scripts -->
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
+
+	
 			<script src="assets/js/main.js"></script>
 
 <?php wp_footer(); ?>
--- wp-content/themes/striped/header.php
+++ wp-content/themes/striped/header.php
@@ -1,13 +1,7 @@
 <!DOCTYPE HTML>
-<!--
-	Striped by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
+<html <?php language_attributes(); ?>>
 	<head>
-		<title>Striped by HTML5 UP</title>
-		<meta charset="utf-8" />
+		<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<?php wp_head(); ?>
 	</head>
